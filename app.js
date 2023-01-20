774ac2b165e00f829d5295f07abe65758fd1f0e3
Athenea Reche Enriquez <atheneareche@gmail.com>
2023-01-20 14:28:51 +0100

    Actualización 20-01-2023. Corrección errores mínimos.

--- app.js
+++ app.js
@@ -38,3 +38,16 @@ theToggle.onclick = function() {
    toggleClass(this, 'on');
    return false;
 }
+
+//dropdown de EKC23
+const dropdown = document.getElementById("dropdown");
+const options = document.getElementById("dropdown-options");
+
+dropdown.addEventListener("mouseover", function() {
+  options.style.display = "block";
+});
+
+dropdown.addEventListener("mouseout", function() {
+  options.style.display = "none";
+});
+
