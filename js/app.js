350be60690c5af763ef48d8b177ffca6cfab6b65
Athenea Reche Enriquez <atheneareche@gmail.com>
2023-02-10 09:56:46 +0100

    Actualización 10-02-23. Incorporación elementos pag principal.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 
+// reset a gif in javascript
+const img = document.getElementById("hero");
+img.src = "resources/main/Conhuellas Fondo blanco.gif"+"?a="+Math.random();
 
 //dropdown de la barra de navegación EKC23
 const dropdown = document.getElementById("dropdown");
